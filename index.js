36b1dce0826cd7f6c22531d17c29bc6e8e5bbcd1
shravan2524 <shravanchenna6@gmail.com>
2022-04-20 19:49:25 +0530

    make it better

--- index.js
+++ index.js
@@ -289,6 +289,10 @@ app.post('/slot', (req, res) => {
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build",     
+      "index.html"));
+    });
 }
 
 
