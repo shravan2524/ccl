495ba39bc1a78aad4a40ba43f1afa35c3281fdd0
shravan2524 <shravanchenna6@gmail.com>
2022-04-20 18:25:20 +0530

    make it better

--- index.js
+++ index.js
@@ -285,16 +285,16 @@ app.post('/slot', (req, res) => {
     .catch(err => console.log(err));
 });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("client/build"));
+// }
 
 
-// app.use(express.static('./client/build'));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build",     
-//   "index.html"));
-// });
+app.use(express.static('./client/build'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build",     
+  "index.html"));
+});
 
 
 // app.set('port', process.env.PORT  || 5000);
